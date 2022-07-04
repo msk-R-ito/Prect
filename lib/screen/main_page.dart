1fd703a4f2a69b8becf6390e1a45a7ee86277e81
msk-R-ito <haruman860@icloud.com>
2022-07-04 21:28:31 +0900

    メインのUIの構築

--- lib/screen/main_page.dart
+++ lib/screen/main_page.dart
@@ -11,9 +11,52 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenHigh = MediaQuery.of(context).size.height;
+    List<Widget> connectDevices = [];
+
     return Scaffold(
-      body: Center(
-      ),
+      body: Container(
+        margin: const EdgeInsets.only(top: 48, left: 16, right: 16),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Card(
+                margin: const EdgeInsets.only(top: 16),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: screenHigh/4,
+                  child: Center(
+                    child: Text('ここにグラフ'),
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 16),
+                child: Row(
+                  children: [
+                    const Text('デバイス情報', style: TextStyle(fontSize: 20),),
+                    const Spacer(),
+                    IconButton(
+                      onPressed: () {
+                        // 画面を接続ページに遷移させる
+                      },
+                      icon: const Icon(Icons.add),
+                    ),
+                  ],
+                ),
+              ),
+              connectDevices.isNotEmpty
+                  ? ListView.builder(
+                itemCount: connectDevices.length,
+                itemBuilder: (context, index) {
+                  return connectDevices[index];
+                },
+              ) : const Text('接続されたデバイスが存在しません'),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
